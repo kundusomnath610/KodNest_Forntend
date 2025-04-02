bafd955e2a868b526c24230d529eeddef2025f50
Somnath Kundu <kundusomnath610@gmail.com>
2025-04-02 10:52:00 +0530

    initial commit

---
+++ redux-App/src/component/userInfo.jsx
@@ -0,0 +1,19 @@
+import { useSelector, useDispatch } from "react-redux"
+import {addMoney, debitMoney} from '../Store'
+export default function UserInfo() {
+    let data = useSelector(state => state.user);
+    const dispatch = useDispatch();
+
+  return (
+    <div>
+        <h1>{data.username}, {data.balance}</h1>
+        <button onClick={()=> dispatch(addMoney(500))}>
+            Add 500
+        </button>
+
+        <button onClick={()=> dispatch(debitMoney(500))}>
+           Debit 500
+        </button>
+    </div>
+  )
+}

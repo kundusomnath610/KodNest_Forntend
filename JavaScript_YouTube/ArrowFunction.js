50e26a4a7e042cefdf7d98e06498091359aeb7b2
Somnath Kundu <kundusomnath610@gmail.com>
2025-06-14 18:19:20 +0530

    commit

---
+++ JavaScript_YouTube/ArrowFunction.js
@@ -0,0 +1,17 @@
+console.log("Hello world");
+
+const heyWorld = () => {
+    console.log("From Arrow function");
+}
+
+heyWorld();
+
+
+var add = (a, b) => a + b;
+console.log(add(20, 30));
+
+const sayHey = () => {
+    console.log("Hello !!!");
+}
+
+//sayHey();

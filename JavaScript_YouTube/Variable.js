1fbf10db3e903518376e2f6ce108b0922328adce
Somnath Kundu <kundusomnath610@gmail.com>
2025-06-14 19:08:05 +0530

    commit

---
+++ JavaScript_YouTube/Variable.js
@@ -0,0 +1,13 @@
+const accountId = 1238874;
+
+let names = "Somnath";
+let password = "Somnath_1234";
+
+let accountState;
+
+
+// console.log(accountId);
+// console.log(names);
+// console.log(password);
+
+console.table([accountId, names, password, accountState]);
